d97ef9f763a0434d3c7e0ba60f03912809e05790
IlyessAgg <ilyessagg@gmail.com>
2026-06-28 11:11:01 +0000

    Added Docker integration

--- api/main.py
+++ api/main.py
@@ -6,12 +6,6 @@ from api.routers.journal_router import router as journal_router
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 logging.info("Journal API starting up")
-# TODO (Task 1): Configure logging here.
-# Reference: https://docs.python.org/3/howto/logging.html
-# Steps:
-#   1. ``import logging`` at the top of this file.
-#   2. Call ``logging.basicConfig(level=logging.INFO, format="...")``.
-#   3. Log an INFO message on startup (e.g. "Journal API starting up").
 
 app = FastAPI(
     title="Journal API",
